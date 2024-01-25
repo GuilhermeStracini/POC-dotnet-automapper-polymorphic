019fb0990bb6b5faac3caf3142a8003b990b4cb7
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-01-25 14:43:44 +0000

    Add automations, tests, linters (#1)
    * Add automations, tests, linters
    
    * Fix tests project

--- Tests/POCAutomapperPolymorphic.Tests/SerializerTests.cs
+++ Tests/POCAutomapperPolymorphic.Tests/SerializerTests.cs
@@ -1,4 +1,4 @@
-using AutoFixture;
+using System.Text.Json;
 using AutoMapper;
 using FluentAssertions;
 using POCAutomapperPolymorphic.Dtos;
@@ -7,12 +7,13 @@ using POCAutomapperPolymorphic.Profiles;
 
 namespace POCAutomapperPolymorphic.Tests;
 
-public class MapperTests
+public class SerializerTests
 {
-    private readonly Fixture _fixture = new();
     private readonly IMapper _mapper;
 
-    public MapperTests()
+    private readonly JsonSerializerOptions _options = new() { WriteIndented = true };
+
+    public SerializerTests()
     {
         var config = new MapperConfiguration(cfg => cfg.AddProfile<MapperProfile>());
         config.AssertConfigurationIsValid();
@@ -20,25 +21,41 @@ public class MapperTests
     }
 
     [Fact]
-    public void MapperShouldMapBaseClass()
+    public void SerializeBaseClass()
     {
         // Arrange
+        const string expectedJson =
+            @"{
+  ""IntProperty"": 1,
+  ""StringProperty"": ""base"",
+  ""InnerProperty"": {
+    ""GuidProperty"": ""00000000-0000-0000-0000-000000000000"",
+    ""StringProperty"": ""inner"",
+    ""DateOnlyProperty"": ""0001-01-01"",
+    ""DateTimeOffsetProperty"": ""1970-01-01T00:00:00+00:00""
+  },
+  ""DerivedProperty"": {
+    ""$type"": ""base"",
+    ""GuidProperty"": ""00000000-0000-0000-0000-000000000000""
+  }
+}";
         var baseModel = new BaseModel
         {
-            IntProperty = _fixture.Create<int>(),
-            StringProperty = _fixture.Create<string>(),
+            IntProperty = 1,
+            StringProperty = "base",
             InnerProperty = new InnerModel
             {
-                GuidProperty = _fixture.Create<Guid>(),
-                StringProperty = _fixture.Create<string>(),
-                DateOnlyProperty = DateOnly.FromDateTime(_fixture.Create<DateTime>()),
-                DateTimeOffsetProperty = _fixture.Create<DateTimeOffset>(),
+                GuidProperty = Guid.Empty,
+                StringProperty = "inner",
+                DateOnlyProperty = DateOnly.FromDayNumber(0),
+                DateTimeOffsetProperty = DateTimeOffset.UnixEpoch,
             },
-            DerivedProperty = new DerivedBaseModel { GuidProperty = _fixture.Create<Guid>() }
+            DerivedProperty = new DerivedBaseModel { GuidProperty = Guid.Empty }
         };
 
         // Act
         var dto = _mapper.Map<BaseDto>(baseModel);
+        var json = JsonSerializer.Serialize(dto, _options);
 
         // Assert
         dto.Should().NotBeNull();
@@ -48,40 +65,56 @@ public class MapperTests
         dto.InnerProperty.GuidProperty.Should().Be(baseModel.InnerProperty.GuidProperty);
         dto.InnerProperty.StringProperty.Should().Be(baseModel.InnerProperty.StringProperty);
         dto.InnerProperty.DateOnlyProperty.Should().Be(baseModel.InnerProperty.DateOnlyProperty);
-        dto.InnerProperty.DateTimeOffsetProperty.Should()
+        dto
+            .InnerProperty.DateTimeOffsetProperty.Should()
             .Be(baseModel.InnerProperty.DateTimeOffsetProperty);
         dto.DerivedProperty.Should().NotBeNull();
         dto.DerivedProperty.Should().BeAssignableTo<DerivedBaseDto>();
         dto.DerivedProperty.Should().NotBeAssignableTo<DerivedADto>();
         dto.DerivedProperty.Should().NotBeAssignableTo<DerivedBDto>();
         dto.DerivedProperty.GuidProperty.Should().Be(baseModel.DerivedProperty.GuidProperty);
+
+        json.Should().NotBeNullOrEmpty();
+        json.Should().Be(expectedJson);
     }
 
     [Fact]
     public void MapperShouldMapDerivedA()
     {
-        // Arrange
-        var derivedAModel = new DerivedAModel
-        {
-            GuidProperty = _fixture.Create<Guid>(),
-            AProperty = _fixture.Create<string>()
-        };
+        const string expectedJson =
+            @"{
+  ""IntProperty"": 1,
+  ""StringProperty"": ""base"",
+  ""InnerProperty"": {
+    ""GuidProperty"": ""00000000-0000-0000-0000-000000000000"",
+    ""StringProperty"": ""inner"",
+    ""DateOnlyProperty"": ""0001-01-01"",
+    ""DateTimeOffsetProperty"": ""1970-01-01T00:00:00+00:00""
+  },
+  ""DerivedProperty"": {
+    ""$type"": ""derivedA"",
+    ""AProperty"": ""A"",
+    ""GuidProperty"": ""00000000-0000-0000-0000-000000000000""
+  }
+}";
+        var derivedAModel = new DerivedAModel { GuidProperty = Guid.Empty, AProperty = "A" };
         var baseModel = new BaseModel
         {
-            IntProperty = _fixture.Create<int>(),
-            StringProperty = _fixture.Create<string>(),
+            IntProperty = 1,
+            StringProperty = "base",
             InnerProperty = new InnerModel
             {
-                GuidProperty = _fixture.Create<Guid>(),
-                StringProperty = _fixture.Create<string>(),
-                DateOnlyProperty = DateOnly.FromDateTime(_fixture.Create<DateTime>()),
-                DateTimeOffsetProperty = _fixture.Create<DateTimeOffset>(),
+                GuidProperty = Guid.Empty,
+                StringProperty = "inner",
+                DateOnlyProperty = DateOnly.FromDayNumber(0),
+                DateTimeOffsetProperty = DateTimeOffset.UnixEpoch,
             },
             DerivedProperty = derivedAModel
         };
 
         // Act
         var dto = _mapper.Map<BaseDto>(baseModel);
+        var json = JsonSerializer.Serialize(dto, _options);
 
         // Assert
         dto.Should().NotBeNull();
@@ -91,7 +124,8 @@ public class MapperTests
         dto.InnerProperty.GuidProperty.Should().Be(baseModel.InnerProperty.GuidProperty);
         dto.InnerProperty.StringProperty.Should().Be(baseModel.InnerProperty.StringProperty);
         dto.InnerProperty.DateOnlyProperty.Should().Be(baseModel.InnerProperty.DateOnlyProperty);
-        dto.InnerProperty.DateTimeOffsetProperty.Should()
+        dto
+            .InnerProperty.DateTimeOffsetProperty.Should()
             .Be(baseModel.InnerProperty.DateTimeOffsetProperty);
         dto.DerivedProperty.Should().NotBeNull();
         dto.DerivedProperty.Should().BeAssignableTo<DerivedBaseDto>();
@@ -102,34 +136,54 @@ public class MapperTests
         derivedADto.Should().NotBeNull();
         derivedADto.GuidProperty.Should().Be(derivedAModel.GuidProperty);
         derivedADto.AProperty.Should().Be(derivedAModel.AProperty);
+
+        json.Should().NotBeNullOrEmpty();
+        json.Should().Be(expectedJson);
     }
 
     [Fact]
     public void MapperShouldMapDerivedB()
     {
-        // Arrange
+        const string expectedJson =
+            @"{
+  ""IntProperty"": 1,
+  ""StringProperty"": ""base"",
+  ""InnerProperty"": {
+    ""GuidProperty"": ""00000000-0000-0000-0000-000000000000"",
+    ""StringProperty"": ""inner"",
+    ""DateOnlyProperty"": ""0001-01-01"",
+    ""DateTimeOffsetProperty"": ""1970-01-01T00:00:00+00:00""
+  },
+  ""DerivedProperty"": {
+    ""$type"": ""derivedB"",
+    ""BProperty"": ""B"",
+    ""BGuidProperty"": ""00000000-0000-0000-0000-000000000000"",
+    ""GuidProperty"": ""00000000-0000-0000-0000-000000000000""
+  }
+}";
         var derivedBModel = new DerivedBModel
         {
-            GuidProperty = _fixture.Create<Guid>(),
-            BProperty = _fixture.Create<string>(),
-            BGuidProperty = _fixture.Create<Guid>()
+            GuidProperty = Guid.Empty,
+            BProperty = "B",
+            BGuidProperty = Guid.Empty
         };
         var baseModel = new BaseModel
         {
-            IntProperty = _fixture.Create<int>(),
-            StringProperty = _fixture.Create<string>(),
+            IntProperty = 1,
+            StringProperty = "base",
             InnerProperty = new InnerModel
             {
-                GuidProperty = _fixture.Create<Guid>(),
-                StringProperty = _fixture.Create<string>(),
-                DateOnlyProperty = DateOnly.FromDateTime(_fixture.Create<DateTime>()),
-                DateTimeOffsetProperty = _fixture.Create<DateTimeOffset>(),
+                GuidProperty = Guid.Empty,
+                StringProperty = "inner",
+                DateOnlyProperty = DateOnly.FromDayNumber(0),
+                DateTimeOffsetProperty = DateTimeOffset.UnixEpoch,
             },
             DerivedProperty = derivedBModel
         };
 
         // Act
         var dto = _mapper.Map<BaseDto>(baseModel);
+        var json = JsonSerializer.Serialize(dto, _options);
 
         // Assert
         dto.Should().NotBeNull();
@@ -139,7 +193,8 @@ public class MapperTests
         dto.InnerProperty.GuidProperty.Should().Be(baseModel.InnerProperty.GuidProperty);
         dto.InnerProperty.StringProperty.Should().Be(baseModel.InnerProperty.StringProperty);
         dto.InnerProperty.DateOnlyProperty.Should().Be(baseModel.InnerProperty.DateOnlyProperty);
-        dto.InnerProperty.DateTimeOffsetProperty.Should()
+        dto
+            .InnerProperty.DateTimeOffsetProperty.Should()
             .Be(baseModel.InnerProperty.DateTimeOffsetProperty);
         dto.DerivedProperty.Should().NotBeNull();
         dto.DerivedProperty.Should().BeAssignableTo<DerivedBaseDto>();
@@ -151,5 +206,8 @@ public class MapperTests
         derivedBDto.GuidProperty.Should().Be(derivedBModel.GuidProperty);
         derivedBDto.BProperty.Should().Be(derivedBModel.BProperty);
         derivedBDto.BGuidProperty.Should().Be(derivedBModel.BGuidProperty);
+
+        json.Should().NotBeNullOrEmpty();
+        json.Should().Be(expectedJson);
     }
 }
